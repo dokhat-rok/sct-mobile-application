134dff029ce48052ee4ff78e99e98401ea53362f
dmitry.krivenko <dima0409.crivenko@yandex.ru>
2023-01-27 18:32:57 +0300

    Редактирование book.md

---
+++ app/src/androidTest/java/com/sct/mobile/application/MyDataBase.java
@@ -0,0 +1,17 @@
+package com.sct.mobile.application;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class MyDataBase extends SQLiteOpenHelper {
+
+    @Override
+    public void onCreate(SQLiteDatabase arg0) {
+    // TODO Auto-generated rnethod stuЬ
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase arg0, int argl, int arg2) {
+    // TODO Auto-generated rnethod stuЬ
+    }
+}
