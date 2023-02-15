c0333ab53104cfcce10eed8402f25ef3a8940ab5
dmitry.krivenko <dima0409.crivenko@yandex.ru>
2023-02-16 01:23:28 +0300

    Добавил активити аренды и вью меню с фильтрами для карты

--- app/src/main/java/com/sct/mobile/application/activity/MapActivity.java
+++ app/src/main/java/com/sct/mobile/application/activity/MapActivity.java
@@ -9,6 +9,7 @@ import androidx.annotation.LayoutRes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.sct.mobile.application.R;
+import com.sct.mobile.application.view.FilterView;
 import com.sct.mobile.application.view.MenuView;
 import com.yandex.mapkit.Animation;
 import com.yandex.mapkit.MapKitFactory;
@@ -20,13 +21,22 @@ public class MapActivity extends AppCompatActivity {
 
     private MapView mapView;
     private MenuView menuView;
-    private LinearLayout layout;
+    private FilterView filterView;
+    private LinearLayout menuLayout;
+    private LinearLayout filterLayout;
+    private static boolean isInitialize = false;
+    private final float animTranslationYBy = 700f;
+    private final int animDuration = 1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        MapKitFactory.setApiKey("22a84de5-deb2-4b2e-883a-246299088e73");
-        MapKitFactory.initialize(this);
+
+        if(!isInitialize){
+            MapKitFactory.setApiKey("22a84de5-deb2-4b2e-883a-246299088e73");
+            MapKitFactory.initialize(this);
+            isInitialize = true;
+        }
 
         setContentView(R.layout.activity_map);
 //        this.findViewById(R.id.map_menu_button).setOnClickListener(this::setupMenu);
@@ -42,10 +52,17 @@ public class MapActivity extends AppCompatActivity {
     @Override
     public void setContentView(@LayoutRes int layoutResID) {
         super.setContentView(layoutResID);
-        layout = findViewById(R.id.map_menu);
-        menuView = new MenuView(getApplicationContext(),layout);
-        layout.addView(menuView);
-        layout.findViewById(R.id.menu_profile_button).setOnClickListener(this::onProfileCLick);
+        menuLayout = findViewById(R.id.map_menu);
+        menuView = new MenuView(getApplicationContext(), menuLayout);
+        menuLayout.addView(menuView);
+        menuLayout.findViewById(R.id.menu_profile_button).setOnClickListener(this::onProfileCLick);
+        menuLayout.findViewById(R.id.menu_rent_button).setOnClickListener(this::onRentClick);
+        menuLayout.findViewById(R.id.menu_filter_button).setOnClickListener(this::onFilterClick);
+
+        filterLayout = findViewById(R.id.map_menu_filter);
+        filterView = new FilterView(getApplicationContext(), filterLayout);
+        filterLayout.findViewById(R.id.filter_remove_button).setOnClickListener(this::onRemoveFilterClick);
+        filterLayout.setTranslationY(animTranslationYBy);
     }
 
     @Override
@@ -64,5 +81,22 @@ public class MapActivity extends AppCompatActivity {
 
     public void onProfileCLick(View view){
         this.startActivity(new Intent(MapActivity.this, ProfileActivity.class));
+    }
+
+    public void onRentClick(View view){
+        this.startActivity(new Intent(MapActivity.this, RentActivity.class));
+    }
+
+    public void onFilterClick(View view){
+        filterLayout.animate().translationYBy(-animTranslationYBy).setDuration(animDuration);
+
+        if(filterLayout.getTranslationY() < animTranslationYBy)
+            filterLayout.animate()
+                    .translationYBy(animTranslationYBy - filterLayout.getTranslationY())
+                    .setDuration(animDuration);
+    }
+
+    public void onRemoveFilterClick(View view){
+        filterLayout.animate().translationYBy(animTranslationYBy).setDuration(animDuration);
     }
 }
--- app/src/main/java/com/sct/mobile/application/view/FilterView.java
+++ app/src/main/java/com/sct/mobile/application/view/FilterView.java
@@ -8,15 +8,15 @@ import android.widget.LinearLayout;
 import com.sct.mobile.application.R;
 
 @SuppressLint("ViewConstructor")
-public class MenuView extends LinearLayout {
+public class FilterView extends LinearLayout {
 
-
-    public MenuView(Context context, LinearLayout parent) {
+    public FilterView(Context context, LinearLayout parent) {
         super(context);
         initView(context, parent);
     }
 
-    public void initView(final Context context,LinearLayout parent){
-        LayoutInflater.from(context).inflate(R.layout.view_menu,parent,true);
+    @SuppressLint("MissingInflatedId")
+    public void initView(final Context context, LinearLayout parent){
+        LayoutInflater.from(context).inflate(R.layout.view_filter_menu,parent,true);
     }
 }
