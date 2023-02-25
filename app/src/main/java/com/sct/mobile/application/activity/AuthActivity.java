93cc6d14bb3ef18de9bee4323bf651070cf84218
dmitry.krivenko <dima0409.crivenko@yandex.ru>
2023-02-26 00:26:13 +0300

    Добавил уведомления об авторизации и регистрации

--- app/src/main/java/com/sct/mobile/application/activity/AuthActivity.java
+++ app/src/main/java/com/sct/mobile/application/activity/AuthActivity.java
@@ -3,6 +3,7 @@ package com.sct.mobile.application.activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,17 +11,23 @@ import com.sct.mobile.application.R;
 
 public class AuthActivity extends AppCompatActivity {
 
+    private Toast toastEntry;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.setContentView(R.layout.activity_auth);
         this.findViewById(R.id.auth_reg_button).setOnClickListener(this::clickOnReg);
         this.findViewById(R.id.auth_entry_button).setOnClickListener(this::clickOnEntry);
+
+        toastEntry = new Toast(this.getBaseContext());
+        toastEntry.setText("Авторизация выполнена");
     }
 
 
     public void clickOnEntry(View view){
         this.startActivity(new Intent(AuthActivity.this, MapActivity.class));
+        toastEntry.show();
     }
 
     public void clickOnReg(View view){
