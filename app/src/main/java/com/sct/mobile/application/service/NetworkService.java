8914ca521ace8cade256c80d9191594b1114189b
dmitry.krivenko <dima0409.crivenko@yandex.ru>
2023-03-04 02:05:16 +0300

    Добавил логику экрану загрзуки и переместил NetworkService

--- app/src/main/java/com/sct/mobile/application/service/NetworkService.java
+++ app/src/main/java/com/sct/mobile/application/service/NetworkService.java
@@ -1,6 +1,10 @@
-package com.sct.mobile.application.config;
+package com.sct.mobile.application.service;
 
 import com.sct.mobile.application.client.AuthApi;
+import com.sct.mobile.application.client.UserApi;
+import com.sct.mobile.application.client.interceptor.AuthHeaderInterceptor;
+import com.sct.mobile.application.client.interceptor.SctApiHeaderInterceptor;
+import com.sct.mobile.application.config.ApiConfig;
 
 import java.util.concurrent.TimeUnit;
 
@@ -14,18 +18,30 @@ public class NetworkService {
     private static NetworkService instance;
 
     private final Retrofit authRetrofit;
+    private final Retrofit apiRetrofit;
 
     private NetworkService() {
 
         HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+        AuthHeaderInterceptor authHeaderInterceptor = new AuthHeaderInterceptor();
+        SctApiHeaderInterceptor sctApiHeaderInterceptor = new SctApiHeaderInterceptor();
         interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
         OkHttpClient.Builder client = new OkHttpClient.Builder()
                 .readTimeout(5, TimeUnit.SECONDS)
                 .connectTimeout(5, TimeUnit.SECONDS)
-                .addInterceptor(interceptor);
+                .addInterceptor(interceptor)
+                .addInterceptor(authHeaderInterceptor);
 
         authRetrofit = new Retrofit.Builder()
-                .baseUrl(ApiConfig.AUTH_URL)
+                .baseUrl(ApiConfig.SCT_AUTH_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(client.build())
+                .build();
+
+        client.interceptors().clear();
+        client.addInterceptor(interceptor).addInterceptor(sctApiHeaderInterceptor);
+        apiRetrofit = new Retrofit.Builder()
+                .baseUrl(ApiConfig.SCT_API_URL)
                 .addConverterFactory(GsonConverterFactory.create())
                 .client(client.build())
                 .build();
@@ -40,5 +56,9 @@ public class NetworkService {
 
     public AuthApi getAuthApi(){
         return authRetrofit.create(AuthApi.class);
+    }
+
+    public UserApi getUserApi(){
+        return apiRetrofit.create(UserApi.class);
     }
 }
